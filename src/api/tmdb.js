29808e4f472f50bc78a01e9875e91f14eacfda97
Idin Iskandar <167355867+idincodingweb@users.noreply.github.com>
2024-12-02 06:26:07 +0700

    Update tmdb.js

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmU2YjgzNjllM2FjMjhjNzNjMjdhM2U4ZjRmZTZmYSIsIm5iZiI6MTczMzA3MTUyOC40MzYsInN1YiI6IjY3NGM5MmE4YjU1NDdhZDk4MDdjZTIzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IS9vCwIb2SFSM1Hvt_KVjbuzNaylviOxp9LXkz3L9No';
+
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_KEY}`
+    Authorization: `Bearer ${API_KEY}`
   }
 });
 
