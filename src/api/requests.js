abe3623d3bc4aad9586fb91761b92e4de4e0165e
Idin Iskandar <167355867+idincodingweb@users.noreply.github.com>
2024-12-02 15:42:52 +0700

    Update requests.js

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,5 +1,3 @@
-const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmU2YjgzNjllM2FjMjhjNzNjMjdhM2U4ZjRmZTZmYSIsIm5iZiI6MTczMzA3MTUyOC40MzYsInN1YiI6IjY3NGM5MmE4YjU1NDdhZDk4MDdjZTIzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IS9vCwIb2SFSM1Hvt_KVjbuzNaylviOxp9LXkz3L9No';
-
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
